3f23bbce653b45511d36dddb1cc92a1ddb3e4b9f
Steven-Mendoza <stevenmendoza2103@gmail.com>
2023-03-27 20:08:37 -0300

    Cambio bienvenida

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let Nombre= prompt ("Hola soy Steven Mendoza, me podrias indicar tu nombre?");
+
+let divbienvenida = document.getElementById("bienvenida");
+
+divbienvenida.innerHTML= `¡Bienvenid@ a mi pagina ${Nombre}!`;
